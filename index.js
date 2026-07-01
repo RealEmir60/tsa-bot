bea04f516a4afda6e975e4f7ac3db91a2500428c
RealEmir60 <158131537+RealEmir60@users.noreply.github.com>
2026-07-01 12:05:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const fetch = require("node-fetch");
+const express = require("express");
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
   ]
 });
 
+// Render açık kalsın diye mini server
+const app = express();
+app.get("/", (req, res) => res.send("TSA BOT AKTİF"));
+app.listen(3000);
+
+// BOT
 client.on("ready", () => {
-  console.log(`BOT GİRİŞ YAPTI: ${client.user.tag}`);
+  console.log("BOT GİRİŞ YAPTI:", client.user.tag);
 });
 
-// basit test komutu
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
 
   if (message.content === "!ping") {
-    message.reply("TSA sistem aktif ✔ Pong!");
-  }
-
-  if (message.content === "!rank") {
-    message.reply("Rütbe sistemi yakında aktif olacak.");
+    message.reply("TSA bot çalışıyor ✔");
   }
 });
+
 client.login(process.env.TOKEN);
-setInterval(() => {
-  console.log("TSA bot aktif...");
-}, 60000);
